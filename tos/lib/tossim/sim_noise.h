fad51d10a27d4b4ebe2772950915d52fe4345482
scipio <scipio>
2007-04-10 22:15:39 +0000

    Fixed memory bug.

--- tos/lib/tossim/sim_noise.h
+++ tos/lib/tossim/sim_noise.h
@@ -41,9 +41,9 @@ extern "C" {
 #endif
 
 enum {
-  NOISE_MIN = -100,
-  NOISE_MAX = -30,
-  NOISE_MIN_QUANTIZE = -100,
+  NOISE_MIN = -115,
+  NOISE_MAX = -5,
+  NOISE_MIN_QUANTIZE = -115,
   NOISE_QUANTIZE_INTERVAL = 5,
   NOISE_BIN_SIZE = (NOISE_MAX - NOISE_MIN)/NOISE_QUANTIZE_INTERVAL,
   NOISE_HISTORY = 20,
@@ -70,6 +70,7 @@ typedef struct sim_noise_node_t {
   char* noiseTrace;
   uint32_t noiseTraceLen;
   uint32_t noiseTraceIndex;
+  bool generated;
 } sim_noise_node_t;
 
 void sim_noise_init();
