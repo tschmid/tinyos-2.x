9cc61e6f53346c35cd9a1661dc7fa287b64de3fc
scipio <scipio>
2008-05-20 21:46:21 +0000

    ENOMEM

--- tos/types/TinyError.h
+++ tos/types/TinyError.h
@@ -38,16 +38,18 @@
 #endif
 
 enum {
-  SUCCESS        = 0,          
-  FAIL           = 1,           // Generic condition: backwards compatible
-  ESIZE          = 2,           // Parameter passed in was too big.
-  ECANCEL        = 3,           // Operation cancelled by a call.
-  EOFF           = 4,           // Subsystem is not active
-  EBUSY          = 5,           // The underlying system is busy; retry later
-  EINVAL         = 6,           // An invalid parameter was passed
-  ERETRY         = 7,           // A rare and transient failure: can retry
-  ERESERVE       = 8,           // Reservation required before usage
-  EALREADY       = 9,           // The device state you are requesting is already set
+  SUCCESS        =  0,          
+  FAIL           =  1,           // Generic condition: backwards compatible
+  ESIZE          =  2,           // Parameter passed in was too big.
+  ECANCEL        =  3,           // Operation cancelled by a call.
+  EOFF           =  4,           // Subsystem is not active
+  EBUSY          =  5,           // The underlying system is busy; retry later
+  EINVAL         =  6,           // An invalid parameter was passed
+  ERETRY         =  7,           // A rare and transient failure: can retry
+  ERESERVE       =  8,           // Reservation required before usage
+  EALREADY       =  9,           // The device state you are requesting is already set
+  ENOMEM         = 10,           // Memory required not available
+  ELAST          = 10            // Last enum value
 };
 
 typedef uint8_t error_t NESC_COMBINE("ecombine");
