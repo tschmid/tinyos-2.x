9e3705ba405a4057c9a2ef2984a1d709bf010473
scipio <scipio>
2008-08-15 18:34:36 +0000

    Cast away const to get rid of warnings.

--- tos/lib/tossim/tossim.c
+++ tos/lib/tossim/tossim.c
@@ -113,8 +113,8 @@ variable_string_t Variable::getData() {
     memcpy(data, ptr, len);
   }
   else {
-    str.ptr = "<no such variable>";
-    str.type = "<no such variable>";
+    str.ptr = (char*)"<no such variable>";
+    str.type = (char*)"<no such variable>";
     str.len = strlen("<no such variable>");
     str.isArray = 0;
   }
@@ -166,7 +166,7 @@ void Mote::setID(unsigned long val) {
 }
 
 Variable* Mote::getVariable(char* name) {
-  char* typeStr = "";
+  char* typeStr = (char*)"";
   int isArray;
   Variable* var;
   
