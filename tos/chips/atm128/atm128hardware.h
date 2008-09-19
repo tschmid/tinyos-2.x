ecbecc3c16e41fe3cf696c85b828c4bba22d5b52
idgay <idgay>
2008-09-19 23:59:00 +0000

    nx_float support

--- tos/chips/atm128/atm128hardware.h
+++ tos/chips/atm128/atm128hardware.h
@@ -135,6 +135,18 @@ mcu_power_t mcombine(mcu_power_t m1, mcu_power_t m2) @safe() {
   return (m1 < m2)? m1: m2;
 }
 
+/* Floating-point network-type support */
+typedef float nx_float __attribute__((nx_base_be(afloat)));
+
+inline float __nesc_ntoh_afloat(const void *COUNT(sizeof(float)) source) @safe() {
+  float f;
+  memcpy(&f, source, sizeof(float));
+  return f;
+}
+
+inline float __nesc_hton_afloat(void *COUNT(sizeof(float)) target, float value) @safe() {
+  memcpy(target, &value, sizeof(float));
+  return value;
+}
+
 #endif //_H_atmega128hardware_H
-
-
