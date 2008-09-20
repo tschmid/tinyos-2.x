cdf278b6057cacffcbf1940bd8fed63cb23c9242
idgay <idgay>
2008-09-20 00:10:18 +0000

    nx_float support

--- tos/chips/atm128/atm128hardware.h
+++ tos/chips/atm128/atm128hardware.h
@@ -135,7 +135,12 @@ mcu_power_t mcombine(mcu_power_t m1, mcu_power_t m2) @safe() {
   return (m1 < m2)? m1: m2;
 }
 
-/* Floating-point network-type support */
+/* Floating-point network-type support.
+   These functions must convert to/from a 32-bit big-endian integer that follows
+   the layout of Java's java.lang.float.floatToRawIntBits method.
+   Conveniently, for the AVR family, this is a straight byte copy...
+*/
+
 typedef float nx_float __attribute__((nx_base_be(afloat)));
 
 inline float __nesc_ntoh_afloat(const void *COUNT(sizeof(float)) source) @safe() {
