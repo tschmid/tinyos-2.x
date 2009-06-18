db43b188f68a9528d5cb38e72df33f7cefe6c2e6
Thomas Schmid <thomas.schmid@gmail.com>
2009-06-18 13:48:11 -0700

    adding files for stm32 programming. these are mainly modified copies from the intel imote2 toolchain.

---
+++ setup.sh
@@ -0,0 +1,7 @@
+export TOSROOT=`pwd`
+export TOSDIR="$TOSROOT/tos"
+export CLASSPATH=$TOSROOT/support/sdk/java:$TOSROOT/support/sdk/java/tinyos.jar:$CLASSPATH
+export MAKERULES="$TOSROOT/support/make/Makerules"
+export PATH=$TOSROOT/tools/mni/scripts:$PATH
+export PYTHONPATH=$TOSROOT/support/sdk/python
+
