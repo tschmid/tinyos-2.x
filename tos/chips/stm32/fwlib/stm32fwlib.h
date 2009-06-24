6e10d0fea8ccd8e579761898aad7f62b11b134ae
Thomas Schmid <thomas.schmid@gmail.com>
2009-06-23 19:02:53 -0700

    Fixed some linker problems to properly place the libc stuff in memory. Now, the NullLed application actually works!!!

--- tos/chips/stm32/fwlib/stm32fwlib.h
+++ tos/chips/stm32/fwlib/stm32fwlib.h
@@ -5,7 +5,7 @@
 #include "stm32f10x_lib.h"
 
 #define BB_BIT(REGISTER, BIT_NUMBER, BASE, BB_BASE) \
-	((volatile s32*) ((BB_BASE) + (((uint32_t) &(REGISTER)) - (BASE)) * 32 + (BIT_NUMBER) * 4))
+	((volatile int32_t*) ((BB_BASE) + (((uint32_t) &(REGISTER)) - (BASE)) * 32 + (BIT_NUMBER) * 4))
 
 #define PERIPHERAL_BIT(REGISTER, BIT_NUMBER) \
 	BB_BIT(REGISTER, BIT_NUMBER, PERIPH_BASE, PERIPH_BB_BASE)
