146ed8587ebf36bba926ed1e1ccd8086d6285e67
wanja <wanja@cs.fau.de>
2009-07-27 15:24:31 -0700

    Added board-specific IRQ handlers. Fixed a naming bug.

--- tos/platforms/sam3u-ek/vectors.c
+++ tos/platforms/sam3u-ek/vectors.c
@@ -11,15 +11,45 @@ void DefaultHandler()
  * handler definition will override this.
  */
 
-void NmiHandler() __attribute__((weak, alias ("DefaultHandler")));
-void HardFaultHandler() __attribute__((weak, alias ("DefaultHandler")));
-void MpuFaultHandler() __attribute__((weak, alias ("DefaultHandler")));
-void BusFaultHandler() __attribute__((weak, alias ("DefaultHandler")));
-void UsageFaultHandler() __attribute__((weak, alias ("DefaultHandler")));
-void SVCallFaultHandler() __attribute__((weak, alias ("DefaultHandler")));
-void DebugHandler() __attribute__((weak, alias ("DefaultHandler")));
-void PendSVHandler() __attribute__((weak, alias ("DefaultHandler")));
-void SysTickHandler() __attribute__((weak, alias ("DefaultHandler")));
+void NmiHandler() __attribute__((weak, alias("DefaultHandler")));
+void HardFaultHandler() __attribute__((weak, alias("DefaultHandler")));
+void MpuFaultHandler() __attribute__((weak, alias("DefaultHandler")));
+void BusFaultHandler() __attribute__((weak, alias("DefaultHandler")));
+void UsageFaultHandler() __attribute__((weak, alias("DefaultHandler")));
+void SVCallFaultHandler() __attribute__((weak, alias("DefaultHandler")));
+void DebugHandler() __attribute__((weak, alias("DefaultHandler")));
+void PendSVHandler() __attribute__((weak, alias("DefaultHandler")));
+void SysTickHandler() __attribute__((weak, alias("DefaultHandler")));
+void SUPCIrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void RSTCIrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void RTCIrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void RTTIrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void WDTIrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void PMCIrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void EFC0IrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void EFC1IrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void DBGUIrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void HSMC4IrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void PIOAIrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void PIOBIrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void PIOCIrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void USART0IrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void USART1IrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void USART2IrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void USART3IrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void MCI0IrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void TWI0IrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void TWI1IrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void SPI0IrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void SSC0IrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void TC0IrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void TC1IrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void TC2IrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void PWMIrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void ADCC0IrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void ADCC1IrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void HDMAIrqHandler() __attribute__((weak, alias("DefaultHandler")));
+void UDPDIrqHandler() __attribute__((weak, alias("DefaultHandler")));
 
 __attribute__((section(".vectors"))) unsigned int *__vectors[] = {
 	// Defined in AT91 ARM Cortex-M3 based Microcontrollers, SAM3U Series, Preliminary, p. 78
@@ -40,36 +70,37 @@ __attribute__((section(".vectors"))) unsigned int *__vectors[] = {
 	(unsigned int *) 0, // Reserved
 	(unsigned int *) PendSVHandler,
 	(unsigned int *) SysTickHandler,
-//    // Defined by board
-//    SUPC_IrqHandler,    // 0  SUPPLY CONTROLLER
-//    RSTC_IrqHandler,    // 1  RESET CONTROLLER
-//    RTC_IrqHandler,     // 2  REAL TIME CLOCK
-//    RTT_IrqHandler,     // 3  REAL TIME TIMER
-//    WDT_IrqHandler,     // 4  WATCHDOG TIMER
-//    PMC_IrqHandler,     // 5  PMC
-//    EFC0_IrqHandler,    // 6  EFC0
-//    EFC1_IrqHandler,    // 7  EFC1
-//    DBGU_IrqHandler,    // 8  DBGU
-//    HSMC4_IrqHandler,   // 9  HSMC4
-//    PIOA_IrqHandler,    // 10 Parallel IO Controller A
-//    PIOB_IrqHandler,    // 11 Parallel IO Controller B
-//    PIOC_IrqHandler,    // 12 Parallel IO Controller C
-//    USART0_IrqHandler,  // 13 USART 0
-//    USART1_IrqHandler,  // 14 USART 1
-//    USART2_IrqHandler,  // 15 USART 2
-//    USART3_IrqHandler,  // 16 USART 3
-//    MCI0_IrqHandler,    // 17 Multimedia Card Interface
-//    TWI0_IrqHandler,    // 18 TWI 0
-//    TWI1_IrqHandler,    // 19 TWI 1
-//    SPI0_IrqHandler,    // 20 Serial Peripheral Interface
-//    SSC0_IrqHandler,    // 21 Serial Synchronous Controller 0
-//    TC0_IrqHandler,     // 22 Timer Counter 0
-//    TC1_IrqHandler,     // 23 Timer Counter 1
-//    TC2_IrqHandler,     // 24 Timer Counter 2
-//    PWM_IrqHandler,     // 25 Pulse Width Modulation Controller
-//    ADCC0_IrqHandler,   // 26 ADC controller0
-//    ADCC1_IrqHandler,   // 27 ADC controller1
-//    HDMA_IrqHandler,    // 28 HDMA
-//    UDPD_IrqHandler,   // 29 USB Device High Speed UDP_HS
-//    IrqHandlerNotUsed   // 30 not used
+	// Defined by SAM3U-EK board
+	// See at91lib/boards/at91sam3u-ek/board_cstartup_gnu.c
+	(unsigned int *) SUPCIrqHandler,    // 0  SUPPLY CONTROLLER
+	(unsigned int *) RSTCIrqHandler,    // 1  RESET CONTROLLER
+	(unsigned int *) RTCIrqHandler,     // 2  REAL TIME CLOCK
+	(unsigned int *) RTTIrqHandler,     // 3  REAL TIME TIMER
+	(unsigned int *) WDTIrqHandler,     // 4  WATCHDOG TIMER
+	(unsigned int *) PMCIrqHandler,     // 5  PMC
+	(unsigned int *) EFC0IrqHandler,    // 6  EFC0
+	(unsigned int *) EFC1IrqHandler,    // 7  EFC1
+	(unsigned int *) DBGUIrqHandler,    // 8  DBGU
+	(unsigned int *) HSMC4IrqHandler,   // 9  HSMC4
+	(unsigned int *) PIOAIrqHandler,    // 10 Parallel IO Controller A
+	(unsigned int *) PIOBIrqHandler,    // 11 Parallel IO Controller B
+	(unsigned int *) PIOCIrqHandler,    // 12 Parallel IO Controller C
+	(unsigned int *) USART0IrqHandler,  // 13 USART 0
+	(unsigned int *) USART1IrqHandler,  // 14 USART 1
+	(unsigned int *) USART2IrqHandler,  // 15 USART 2
+	(unsigned int *) USART3IrqHandler,  // 16 USART 3
+	(unsigned int *) MCI0IrqHandler,    // 17 Multimedia Card Interface
+	(unsigned int *) TWI0IrqHandler,    // 18 TWI 0
+	(unsigned int *) TWI1IrqHandler,    // 19 TWI 1
+	(unsigned int *) SPI0IrqHandler,    // 20 Serial Peripheral Interface
+	(unsigned int *) SSC0IrqHandler,    // 21 Serial Synchronous Controller 0
+	(unsigned int *) TC0IrqHandler,     // 22 Timer Counter 0
+	(unsigned int *) TC1IrqHandler,     // 23 Timer Counter 1
+	(unsigned int *) TC2IrqHandler,     // 24 Timer Counter 2
+	(unsigned int *) PWMIrqHandler,     // 25 Pulse Width Modulation Controller
+	(unsigned int *) ADCC0IrqHandler,   // 26 ADC controller0
+	(unsigned int *) ADCC1IrqHandler,   // 27 ADC controller1
+	(unsigned int *) HDMAIrqHandler,    // 28 HDMA
+	(unsigned int *) UDPDIrqHandler,    // 29 USB Device High Speed UDP_HS
+	(unsigned int *) 0 // not used
 };
