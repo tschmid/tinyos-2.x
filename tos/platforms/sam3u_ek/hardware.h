19a090e9e585874177f3e47fb9c27dec4fb54e11
wanja <wanja@cs.fau.de>
2009-07-28 15:07:09 -0700

    Deleted LED test code in __init().

--- tos/platforms/sam3u_ek/hardware.h
+++ tos/platforms/sam3u_ek/hardware.h
@@ -18,14 +18,6 @@ extern unsigned int _estack;
  */
 int main();
 
-volatile unsigned int *PIOB_BASE = (volatile unsigned int *) 0x400e0e00;
-volatile unsigned int *PIOB_PER = (volatile unsigned int *) 0x400e0e00;
-volatile unsigned int *PIOB_OER = (volatile unsigned int *) 0x400e0e10;
-volatile unsigned int *PIOB_SODR = (volatile unsigned int *) 0x400e0e30;
-volatile unsigned int *PIOB_CODR = (volatile unsigned int *) 0x400e0e34;
-volatile unsigned int *PIOB_PUDR = (volatile unsigned int *) 0x400e0e60;
-volatile unsigned int *PIOB_MDDR = (volatile unsigned int *) 0x400e0e54;
-
 /* Start-up code to copy data into RAM
  * and zero BSS segment
  * and call main()
@@ -36,10 +28,6 @@ void __init() @spontaneous()
 	unsigned int *from;
 	unsigned int *to;
 	unsigned int *i;
-#if 0
-	volatile int j;
-	volatile unsigned int value = 0x0;
-#endif
 
 	// Copy pre-initialized data into RAM
 	from = &_etext;
@@ -56,30 +44,6 @@ void __init() @spontaneous()
 		*i = 0;
 		i++;
 	}
-
-#if 0
-	// FIXME
-	// test GPIO and LEDs
-//	*PIOB_PUDR = 0x0007; // disable all pull-up resistors
-//	*PIOB_MDDR = 0x0007; // disable all multi-drives
-//	*PIOB_PER = 0x0007; // enable PB0 and PB1 and PB2 to be controlled by PIO controller
-	*PIOB_OER = 0x0007; // enable PB0 and PB1 and PB2 to be driven by PIO controller
-	*PIOB_SODR = 0x0000; // drive PB0-2 at low level
-	*PIOB_CODR = 0x0007; // drive PB0-2 at low level
-
-	while (1) {
-		for (j = 0; j < 200000; j++);
-		value++;
-		value = value % 2;
-		if (value == 0) {
-			*PIOB_SODR = 0x0003;
-			*PIOB_CODR = 0x0004;
-		} else if (value == 1) {
-			*PIOB_SODR = 0x0004;
-			*PIOB_CODR = 0x0003;
-		}
-	}
-#endif
 
 	// Call main()
 	main();
