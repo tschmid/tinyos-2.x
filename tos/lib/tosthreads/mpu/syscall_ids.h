8115a2630cac4308d7b37af19a3979bd4254d53e
wanja <wanja@cs.fau.de>
2009-12-15 16:19:44 -0800

    Consolidated BlockingStdControl. Test still works.

--- tos/lib/tosthreads/mpu/syscall_ids.h
+++ tos/lib/tosthreads/mpu/syscall_ids.h
@@ -2,6 +2,8 @@
 #define SYSCALL_IDS_H
 
 // 0x0 to 0xf reserved
-#define SYSCALL_ID_READ 0x10
+#define SYSCALL_ID_READ                   0x10
+#define SYSCALL_ID_STDCONTROL_START       0x11
+#define SYSCALL_ID_STDCONTROL_STOP        0x12
 
 #endif // SYSCALL_IDS_H
