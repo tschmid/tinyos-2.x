8544c983aca275e9ec5ceadbddbeaa7d236b431d
wanja <wanja@cs.fau.de>
2009-12-18 10:41:01 -0800

    mpu: Blink_Preemptive can also be run protected. Added this to test script.

--- tools/platforms/sam3u/sam3u-wanja-tests.pl
+++ tools/platforms/sam3u/sam3u-wanja-tests.pl
@@ -6,6 +6,7 @@ my @apps = (
 	{"name" => "apps/tosthreads/apps/Blink", "variant" => "normal", "make" => "make sam3u_ek debug threads", "expected" => "LEDs count up modulo 8"},
 	{"name" => "apps/tosthreads/apps/Blink", "variant" => "protected", "make" => "make sam3u_ek debug threads mpu", "expected" => "LEDs count up modulo 8"},
 	{"name" => "apps/tosthreads/apps/Blink_Preemptive", "variant" => "normal", "make" => "make sam3u_ek debug threads", "expected" => "LEDs all light up, then they all go out, repeatedly (out of sync, but that's OK)"},
+	{"name" => "apps/tosthreads/apps/Blink_Preemptive", "variant" => "protected", "make" => "make sam3u_ek debug threads mpu", "expected" => "LEDs all light up, then they all go out, repeatedly (out of sync, but that's OK)"},
 	{"name" => "apps/tests/TestMpuProtection", "variant" => "normal", "make" => "make sam3u_ek debug threads", "expected" => "LED 0 lights up, then LED 1 (green) lights up (meaning successful write to other thread's data)"},
 	{"name" => "apps/tests/TestMpuProtection", "variant" => "protected", "make" => "make sam3u_ek debug threads mpu", "expected" => "LED 0 lights up, then LED 2 (red) lights up (meaning the MPU trapped)"},
 	{"name" => "apps/tests/TestMpuProtectionSyscall", "variant" => "normal", "make" => "make sam3u_ek debug threads", "expected" => "LED 0 is blinking (meaning it is sending UART packets)"},
