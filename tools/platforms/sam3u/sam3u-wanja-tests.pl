486cf29f451a96fe1abb59d7c37e12849b3ab61e
wanja <wanja@cs.fau.de>
2009-12-27 13:37:36 -0800

    mpu: Moved test apps to appropriate subdir. Adapted test script.

--- tools/platforms/sam3u/sam3u-wanja-tests.pl
+++ tools/platforms/sam3u/sam3u-wanja-tests.pl
@@ -19,12 +19,12 @@ my @apps = (
 	{"name" => "apps/tosthreads/apps/Blink_Preemptive", "variant" => "protected", "make" => "make sam3u_ek debugopt threads mpu", "expected" => "LEDs all light up, then they all go out, repeatedly (out of sync, but that's OK)"},
 	{"name" => "apps/tosthreads/apps/TestJoin", "variant" => "normal", "make" => "make sam3u_ek debugopt threads", "expected" => "LEDs 1-3 blink, then LEDs 2-3 blink, then LED 3 blinks, repeat"},
 	{"name" => "apps/tosthreads/apps/TestJoin", "variant" => "protected", "make" => "make sam3u_ek debugopt threads mpu", "expected" => "LEDs 1-3 blink, then LEDs 2-3 blink, then LED 3 blinks, repeat"},
-	{"name" => "apps/tests/TestRestoreTcb", "variant" => "normal", "make" => "make sam3u_ek debugopt threads", "expected" => "LEDs 1-3 all light up"},
-	{"name" => "apps/tests/TestRestoreTcb", "variant" => "protected", "make" => "make sam3u_ek debugopt threads mpu", "expected" => "LEDs 1-3 all light up"},
-	{"name" => "apps/tests/TestMpuProtection", "variant" => "normal", "make" => "make sam3u_ek debugopt threads", "expected" => "LED 0 lights up, then LED 1 (green) lights up (meaning successful write to other thread's data)"},
-	{"name" => "apps/tests/TestMpuProtection", "variant" => "protected", "make" => "make sam3u_ek debugopt threads mpu", "expected" => "LED 0 lights up, then LED 2 (red) lights up (meaning the MPU trapped)"},
-	{"name" => "apps/tests/TestMpuProtectionSyscall", "variant" => "normal", "make" => "make sam3u_ek debugopt threads", "expected" => "LED 0 is blinking (meaning it is sending UART packets)"},
-	{"name" => "apps/tests/TestMpuProtectionSyscall", "variant" => "protected", "make" => "make sam3u_ek debugopt threads mpu", "expected" => "LED 0 is blinking (meaning it is sending UART packets)"},
+	{"name" => "apps/tosthreads/apps/TestRestoreTcb", "variant" => "normal", "make" => "make sam3u_ek debugopt threads", "expected" => "LEDs 1-3 all light up"},
+	{"name" => "apps/tosthreads/apps/TestRestoreTcb", "variant" => "protected", "make" => "make sam3u_ek debugopt threads mpu", "expected" => "LEDs 1-3 all light up"},
+	{"name" => "apps/mpu/TestMpuProtection", "variant" => "normal", "make" => "make sam3u_ek debugopt threads", "expected" => "LED 0 lights up, then LED 1 (green) lights up (meaning successful write to other thread's data)"},
+	{"name" => "apps/mpu/TestMpuProtection", "variant" => "protected", "make" => "make sam3u_ek debugopt threads mpu", "expected" => "LED 0 lights up, then LED 2 (red) lights up (meaning the MPU trapped)"},
+	{"name" => "apps/mpu/TestMpuProtectionSyscall", "variant" => "normal", "make" => "make sam3u_ek debugopt threads", "expected" => "LED 0 is blinking (meaning it is sending UART packets)"},
+	{"name" => "apps/mpu/TestMpuProtectionSyscall", "variant" => "protected", "make" => "make sam3u_ek debugopt threads mpu", "expected" => "LED 0 is blinking (meaning it is sending UART packets)"},
 );
 
 my $num = 0;
